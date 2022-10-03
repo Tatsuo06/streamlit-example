721cf8bac6a6b02002817983cee23064a1e0b741
Tatsuo06 <54253397+Tatsuo06@users.noreply.github.com>
2022-10-03 14:32:08 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,10 @@
-from collections import namedtuple
-import altair as alt
-import math
-import pandas as pd
+#from collections import namedtuple
+#import altair as alt
+#import math
+#import pandas as pd
 import streamlit as st
 
+st.title("hello")
 """
 # Welcome to Streamlit!
 
@@ -15,7 +16,7 @@ forums](https://discuss.streamlit.io).
 In the meantime, below is an example of what you can do with just a few lines of code:
 """
 
-
+"""
 with st.echo(code_location='below'):
     total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
     num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
@@ -36,3 +37,4 @@ with st.echo(code_location='below'):
     st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
         .mark_circle(color='#0068c9', opacity=0.5)
         .encode(x='x:Q', y='y:Q'))
+"""
